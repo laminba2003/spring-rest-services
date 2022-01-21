2d207e683eaf0bc80d8b1e0d26524d3294a7681e
mamadou lamine ba <laminba2003@gmail.com>
2022-01-21 15:18:53 +0100

    enabling caching

--- src/main/java/com/spring/training/controller/CountryController.java
+++ src/main/java/com/spring/training/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.spring.training.controller;
 import com.spring.training.model.Country;
 import com.spring.training.service.CountryService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class CountryController {
     private CountryService countryService;
 
     @GetMapping
+    @Cacheable("countries")
     public List<Country> getCountries() {
       return countryService.getCountries();
     }
