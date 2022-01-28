a3eb7c25c26c838498c669892a22e5025ae45150
mamadou lamine ba <laminba2003@gmail.com>
2022-01-28 22:27:31 +0100

    improvements

--- src/main/java/com/spring/training/controller/PersonController.java
+++ src/main/java/com/spring/training/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.spring.training.controller;
 import com.spring.training.model.Person;
 import com.spring.training.service.PersonService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class PersonController {
     private final PersonService personService;
 
     @GetMapping
+    @Cacheable("persons")
     public List<Person> getPersons() {
         return personService.getPersons();
     }
--- src/main/java/com/spring/training/service/PersonService.java
+++ src/main/java/com/spring/training/service/PersonService.java
@@ -3,7 +3,6 @@ package com.spring.training.service;
 import com.spring.training.model.Person;
 import com.spring.training.repository.PersonRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +13,6 @@ public class PersonService {
 
     private final PersonRepository personRepository;
 
-    @Cacheable("persons")
     public List<Person> getPersons() {
         return personRepository.findAll().stream().map(entity -> entity.toPerson()).collect(Collectors.toList());
     }
