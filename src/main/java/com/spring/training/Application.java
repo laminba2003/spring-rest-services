12c01117efe7e90fa3b98c2a950a476eeb68d0e5
mamadou lamine ba <laminba2003@gmail.com>
2022-03-17 00:36:37 +0100

    improvements

--- src/main/java/com/spring/training/Application.java
+++ src/main/java/com/spring/training/Application.java
@@ -1,8 +1,11 @@
 package com.spring.training;
 
+import brave.context.log4j2.ThreadContextScopeDecorator;
+import brave.propagation.CurrentTraceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +15,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CurrentTraceContext.ScopeDecorator decorator() {
+		return ThreadContextScopeDecorator.get();
+	}
 }
