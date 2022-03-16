c9f9c26d57ada0238805c3c8beac3cdbdc2e8050
mamadou lamine ba <laminba2003@gmail.com>
2022-03-16 19:05:02 +0100

    improvements

--- src/main/java/com/spring/training/controller/CountryController.java
+++ src/main/java/com/spring/training/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.spring.training.controller;
 import com.spring.training.model.Country;
 import com.spring.training.service.CountryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +13,20 @@ import java.util.List;
 @RestController
 @RequestMapping("countries")
 @AllArgsConstructor
+@Slf4j
 public class CountryController {
 
     private final CountryService countryService;
 
     @GetMapping(produces = "application/json")
     public List<Country> getCountries() {
+        log.debug("returning the list of countries");
         return countryService.getCountries();
     }
 
     @GetMapping(path="{name}", produces = "application/json")
     public Country getCountry(@PathVariable("name") String name) {
+        log.debug("returning the country with name = {}", name);
         return countryService.getCountry(name);
     }
 
--- src/main/java/com/spring/training/controller/PersonController.java
+++ src/main/java/com/spring/training/controller/PersonController.java
@@ -3,27 +3,30 @@ package com.spring.training.controller;
 import com.spring.training.model.Person;
 import com.spring.training.service.PersonService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("persons")
 @AllArgsConstructor
+@Slf4j
 public class PersonController {
 
     private final PersonService personService;
 
     @GetMapping(produces = "application/json")
     public List<Person> getPersons() {
+        log.debug("returning the list of persons");
         return personService.getPersons();
     }
 
     @GetMapping(path = "{id}", produces = "application/json")
     public Person getPerson(@PathVariable("id") Long id) {
+        log.debug("returning the person with id = {}", id);
         return personService.getPerson(id);
     }
 
