ee25969fcdb5e79c3bc2a57580130d898ff56ceb
mamadou lamine ba <laminba2003@gmail.com>
2022-03-19 02:33:28 +0100

    improvements

--- src/main/java/com/spring/training/Application.java
+++ src/main/java/com/spring/training/Application.java
@@ -4,11 +4,13 @@ import brave.context.log4j2.ThreadContextScopeDecorator;
 import brave.propagation.CurrentTraceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
