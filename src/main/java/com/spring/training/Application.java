dee4178f9181ecdc34c5d430650d3fce52b1ca04
mamadou lamine ba <laminba2003@gmail.com>
2022-03-25 00:43:41 +0100

    improvements

--- src/main/java/com/spring/training/Application.java
+++ src/main/java/com/spring/training/Application.java
@@ -2,9 +2,10 @@ package com.spring.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @EnableCaching
 public class Application {
 
