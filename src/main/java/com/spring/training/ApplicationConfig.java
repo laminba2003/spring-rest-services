28a9f626e03c25a2523a0ec8b913ea6f78618bd6
mamadou lamine ba <laminba2003@gmail.com>
2022-03-25 01:16:38 +0100

    improvements

--- src/main/java/com/spring/training/ApplicationConfig.java
+++ src/main/java/com/spring/training/ApplicationConfig.java
@@ -2,15 +2,10 @@ package com.spring.training;
 
 import brave.context.log4j2.ThreadContextScopeDecorator;
 import brave.propagation.CurrentTraceContext;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@ConditionalOnProperty(prefix = "spring", name = "cache.type", havingValue = "redis")
 @Configuration
-@Import({ RedisAutoConfiguration.class })
 public class ApplicationConfig {
 
     @Bean
---
+++ src/main/java/com/spring/training/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.spring.training;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@ConditionalOnProperty(prefix = "spring", name = "cache.type", havingValue = "redis")
+@Configuration
+@Import({ RedisAutoConfiguration.class })
+public class RedisConfig {
+
+}
