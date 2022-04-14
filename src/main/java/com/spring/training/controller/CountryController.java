18ea90719787ac8be5168b4449629743fe9e5fe0
mamadou lamine ba <laminba2003@gmail.com>
2022-04-14 21:06:58 +0000

    improvements

--- src/main/java/com/spring/training/controller/CountryController.java
+++ src/main/java/com/spring/training/controller/CountryController.java
@@ -2,12 +2,9 @@ package com.spring.training.controller;
 
 import com.spring.training.annotation.IsAdmin;
 import com.spring.training.domain.Country;
-import com.spring.training.domain.User;
 import com.spring.training.service.CountryService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
@@ -15,45 +12,36 @@ import java.util.List;
 @RestController
 @RequestMapping("countries")
 @AllArgsConstructor
-@Slf4j
 public class CountryController {
 
     private final CountryService countryService;
 
     @GetMapping
     public List<Country> getCountries() {
-        log.debug("returning the list of countries");
         return countryService.getCountries();
     }
 
     @GetMapping("{name}")
     public Country getCountry(@PathVariable("name") String name) {
-        log.debug("returning the country with name = {}", name);
         return countryService.getCountry(name);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     @IsAdmin
-    public Country createCountry(Authentication authentication, @Valid @RequestBody Country country) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("creating country with values = {} and user : {}", country, user.getEmail());
+    public Country createCountry(@Valid @RequestBody Country country) {
         return countryService.createCountry(country);
     }
 
     @PutMapping("{name}")
     @IsAdmin
-    public Country updateCountry(Authentication authentication, @PathVariable("name") String name, @Valid @RequestBody Country country) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("updating country with name = {}, values = {} and user : {}", name, country, user.getEmail());
+    public Country updateCountry(@PathVariable("name") String name, @Valid @RequestBody Country country) {
         return countryService.updateCountry(name, country);
     }
 
     @DeleteMapping("{name}")
     @IsAdmin
-    public void deleteCountry(Authentication authentication, @PathVariable("name") String name) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("deleting country with name = {} and user : {}", name, user.getEmail());
+    public void deleteCountry(@PathVariable("name") String name) {
         countryService.deleteCountry(name);
     }
 
--- src/main/java/com/spring/training/controller/PersonController.java
+++ src/main/java/com/spring/training/controller/PersonController.java
@@ -2,58 +2,46 @@ package com.spring.training.controller;
 
 import com.spring.training.annotation.IsAdmin;
 import com.spring.training.domain.Person;
-import com.spring.training.domain.User;
 import com.spring.training.service.PersonService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("persons")
 @AllArgsConstructor
-@Slf4j
 public class PersonController {
 
     private final PersonService personService;
 
     @GetMapping
     public Page<Person> getPersons(Pageable pageable) {
-        log.debug("returning the list of persons with {}", pageable);
         return personService.getPersons(pageable);
     }
 
     @GetMapping("{id}")
     public Person getPerson(@PathVariable("id") Long id) {
-        log.debug("returning the person with id = {}", id);
         return personService.getPerson(id);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     @IsAdmin
-    public Person createPerson(Authentication authentication, @Valid @RequestBody Person person) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("creating person with values = {} and user : {}", person, user.getEmail());
+    public Person createPerson(@Valid @RequestBody Person person) {
         return personService.createPerson(person);
     }
 
     @PutMapping("{id}")
     @IsAdmin
-    public Person updatePerson(Authentication authentication, @PathVariable("id") Long id, @Valid @RequestBody Person person) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("updating person with id = {}, values = {} and user : {}", id, person, user.getEmail());
+    public Person updatePerson(@PathVariable("id") Long id, @Valid @RequestBody Person person) {
         return personService.updatePerson(id, person);
     }
 
     @DeleteMapping("{id}")
-    public void deletePerson(Authentication authentication, @PathVariable("id") Long id) {
-        User user = (User) authentication.getPrincipal();
-        log.debug("deleting person with id = {} and user : {}", id, user.getEmail());
+    public void deletePerson(@PathVariable("id") Long id) {
         personService.deletePerson(id);
     }
 
