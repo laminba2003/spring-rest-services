9045a741b95bb951de2754d9eeca00617e307462
mamadou lamine ba <laminba2003@gmail.com>
2022-04-25 21:50:24 +0000

    improvements

--- src/test/java/com/spring/training/ApplicationTests.java
+++ src/test/java/com/spring/training/ApplicationTests.java
@@ -2,30 +2,24 @@ package com.spring.training;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
 	@Container
-	static MySQLContainer mysql;
-
-	static {
-		mysql = new MySQLContainer("mysql:5.7")
+	static MySQLContainer mysql = new MySQLContainer("mysql:5.7")
 				.withDatabaseName("spring_training")
 				.withUsername("user")
 				.withPassword("password");
-		mysql.start();
-	}
 
 	@DynamicPropertySource
-	public static void properties(DynamicPropertyRegistry registry) {
+	static void properties(DynamicPropertyRegistry registry) {
 		registry.add("spring.datasource.url",mysql::getJdbcUrl);
 		registry.add("spring.datasource.username", mysql::getUsername);
 		registry.add("spring.datasource.password", mysql::getPassword);
