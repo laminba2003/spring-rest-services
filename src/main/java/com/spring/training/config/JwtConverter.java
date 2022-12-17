ae3dfdf406bb1579bfd361cd1ca94431042e38b7
mamadou lamine ba <laminba2003@gmail.com>
2022-12-17 20:25:23 +0000

    improvements

--- src/main/java/com/spring/training/config/JwtConverter.java
+++ src/main/java/com/spring/training/config/JwtConverter.java
@@ -1,11 +1,13 @@
 package com.spring.training.config;
 
-import com.spring.training.domain.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 
@@ -14,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.spring.training.config.Claims.*;
+import static com.spring.training.config.Claims.ROLES;
 
 
 public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
@@ -25,12 +27,11 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
         List<GrantedAuthority> authorities = roles.stream().
                 map(authority -> new SimpleGrantedAuthority("ROLE_" + authority))
                 .collect(Collectors.toList());
-        User user = new User(jwt.getClaim(GIVEN_NAME),
-                jwt.getClaim(FAMILY_NAME),
-                jwt.getClaim(EMAIL), authorities);
-        OAuth2AccessToken token = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,
+        OidcIdToken oidcIdToken = new OidcIdToken(jwt.getTokenValue(), jwt.getIssuedAt(), jwt.getExpiresAt(), jwt.getClaims());
+        OidcUser user = new DefaultOidcUser(authorities, oidcIdToken);
+        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,
                 jwt.getTokenValue(), jwt.getIssuedAt(), jwt.getExpiresAt());
-        return new BearerTokenAuthentication(user, token, authorities);
+        return new BearerTokenAuthentication(user, oAuth2AccessToken, authorities);
     }
 
 }
